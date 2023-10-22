937ac6750416d5b3f92569256ad50e35d40d9b37
JeanRD7 <jean64098@gmail.com>
2023-10-22 16:49:44 -0400

    creando el context

---
+++ TicketDetalle/Server/DAL/Context.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TicketDetalle.Server.DAL
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> Opcions) : base(Opcions) { }
+        public DbSet<TicketDetalles> TicketDetalles { get; set; }
+    }
+}
