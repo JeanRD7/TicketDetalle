550352523740a5e4763c891876a0e9586e296ca3
JeanRD7 <jean64098@gmail.com>
2023-10-22 16:58:18 -0400

    creando migracion y base de datos

---
+++ TicketDetalle/Server/Migrations/20231022205520_Inicial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicketDetalle.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TicketDetalles",
+                columns: table => new
+                {
+                    TicketDetalleId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    emisor = table.Column<string>(type: "TEXT", nullable: false),
+                    mensaje = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TicketDetalles", x => x.TicketDetalleId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TicketDetalles");
+        }
+    }
+}
