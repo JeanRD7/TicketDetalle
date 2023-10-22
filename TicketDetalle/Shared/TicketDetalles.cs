9c31ee679a1b39dce079883bb2033c59a4492a3f
JeanRD7 <jean64098@gmail.com>
2023-10-22 16:45:05 -0400

    creando el modelo

---
+++ TicketDetalle/Shared/TicketDetalles.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class TicketDetalles
+{
+    [Key]
+
+    public int TicketDetalleId { get; set; }
+
+    [Required(ErrorMessage = "Debe llenar este campo")]
+
+    public string? emisor { get; set; }
+    [Required(ErrorMessage = "Debe llenar este campo")]
+
+    public string? mensaje { get; set; }
+}
+
